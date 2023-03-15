5d993c729ea19b011ad3eceb20992d7677866917
mokachinon <73018512+mokachinon@users.noreply.github.com>
2023-03-15 12:01:04 +0800

    vercel

--- dist/_nuxt/static/1678798651/user/login/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FBP-nuxt\u002F_nuxt\u002Fstatic\u002F1678798651",layout:"default",error:a,state:{portfolioList:[],portfolioDetail:a,token:a,userData:a},serverRendered:true,routePath:"\u002Fuser\u002Flogin",config:{_app:{basePath:"\u002FBP-nuxt\u002F",assetsPath:"\u002FBP-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
